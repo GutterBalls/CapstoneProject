859ec363f3475b8764207684fcb60308b31536bb
Bryan Runnels <bryan.d.runnels@gmail.com>
2023-04-03 10:57:34 -0600

    Shoes & Bags now updated with current DB seed / pagination / display - all functional

--- src/components/shoes.jsx
+++ src/components/shoes.jsx
@@ -1,17 +1,27 @@
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import ReactPaginate from "react-paginate";
+const perPage = 6;
 
 const Shoes = (props) => {
-
+    const [currentPage, setCurrentPage] = useState(0);
+    const offset = currentPage * perPage;
+    const shoeProducts = props.productData.filter((singleShoe) => singleShoe.category_id === 3);
+    const pageCount = Math.ceil(shoeProducts.length / perPage);
     useEffect(() => {
         props.getProductData();
     }, []);
 
+    function pageClick({ selected: selectedPage}) {
+        setCurrentPage(selectedPage)
+    };
+
     return (
         <div className="homepage">
             <p>Shoes</p>
+            <div className="mainProductFlex">
             {
-                props.productData.length ? props.productData.filter((singleBag) => singleBag.category_id === 3).map((singleProduct) => {
+                props.productData.length ? props.productData.filter((singleShoe) => singleShoe.category_id === 3).slice(offset, offset + perPage).map((singleProduct) => {
                     
                     return (
                         <div key={singleProduct.id} className="singleProduct">
@@ -28,6 +38,19 @@ const Shoes = (props) => {
                     
                 }) : <h1> No data loaded. </h1>
             }
+            </div>
+            <ReactPaginate
+            previousLabel={"Previous"}
+            nextLabel={"Next"}
+            pageCount={pageCount}
+            onPageChange={pageClick}
+            containerClassName={"pagination"}
+            previousLinkClassName={"item previous"}
+            nextLinkClassName={"item next"}
+            disabledClassName={"disabled-page"}
+            activeClassName={"item active"}
+            disabledLinkClassName={"item disabled"}
+            />
         </div>
     )
 }

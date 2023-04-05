c21c53f0a5941d34dd56246355dfb7f04506a119
David Burkhart <dburkhart77@gmail.com>
2023-04-05 12:01:53 -0600

    css and sidebar updates

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,9 +1,8 @@
 const Footer = () => {
 
     return (
-        <div className="">
-            <p>1 - Footer.jsx</p>
-            <p>2</p>
+        <div className="footer">
+            <p> FOOTER SECTION</p>
         </div>
     )
 }
--- src/components/homepage.jsx
+++ src/components/homepage.jsx
@@ -15,12 +15,13 @@ const Homepage = (props) => {
     }, [])
 
     return (
-        <div className="main-container">
-            <h1> Welcome to the Gutterball Bowling Superstore! </h1>
-            <h3> Please login or create an account above to place an order or feel free to browse our selection of products.</h3>
-            <h3> Thank you for your business. </h3>
-            
-        </div>
+        <section className="main-container">
+            <div className="main-full-width">
+                <h1> Welcome to the Gutterball Bowling Superstore! </h1>
+                <h3> Please login or create an account above to place an order or feel free to browse our selection of products.</h3>
+                <h3> Thank you for your business. </h3>
+            </div>           
+        </section>
     )
 }
 export default Homepage;

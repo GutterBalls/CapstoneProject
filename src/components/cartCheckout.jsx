02b0122f56c3cda56b761a02de551d2ce90b6a77
Bryan Runnels <bryan.d.runnels@gmail.com>
2023-04-05 12:58:23 -0600

    Adjustments to cartCheckout.jsx

--- src/components/cartCheckout.jsx
+++ src/components/cartCheckout.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom"
 import {
   MDBBtn,
   MDBCard,
@@ -11,8 +13,62 @@ import {
   MDBRow,
   MDBTypography,
 } from "mdb-react-ui-kit";
+const DATABASE_URL = 'http://localhost:1337/api';
 
-const CartCheckout = () => {
+const CartCheckout = (props) => {
+    const [cartQuantity, setCartQuantity] = useState(1);
+    const [cartData, setCartData] = useState([]);
+    const [totalPrice, setTotalPrice] = useState(0);
+
+    useEffect(() => {
+        getCartData();
+        if (localStorage.getItem("token")) {
+        props.getUserData();
+        };
+    }, []);
+
+    // GET all carts.
+    async function getCartData() {
+        try {
+            const response = await fetch(`${DATABASE_URL}/cartItems`)
+            const translatedData = await response.json();
+            console.log(translatedData);
+
+            setCartData(translatedData);
+            console.log("Cart data", cartData)
+            console.log("Props user data", props.userData.id)
+            return translatedData
+        } catch (error) {
+            console.log(error)
+        };
+    };
+
+    // Setting state for Cart Quantity
+    function quantityAddOrMinus(event) {
+        console.log(event.target.value)
+        setCartQuantity(event.target.value)
+    };
+    
+    // Adding 1 on + click to increase quantity state. 
+    function addQuantity() {
+        console.log("Add was clicked!")
+        setCartQuantity(cartQuantity + 1);
+    };
+
+    // Subtracting 1 on - click to decrease quantity state. 
+    function minusQuantity() {
+        if (cartQuantity > 1) {
+        console.log("Minus was clicked!")
+        setCartQuantity(cartQuantity - 1);
+        };
+    };
+
+    function calculateTotal() {
+
+    };
+
+    
+
   return (
     <section className="h-100 h-custom" style={{ backgroundColor: "#eee" }}>
       <MDBContainer className="h-100 py-5">
@@ -26,13 +82,15 @@ const CartCheckout = () => {
                       tag="h3"
                       className="mb-5 pt-2 text-center fw-bold text-uppercase"
                     >
-                      Your products
+                      Shopping Cart Items
+                    
                     </MDBTypography>
-
-                    <div className="d-flex align-items-center mb-5">
+                    { cartData.length > 0 && localStorage.getItem("token") ? cartData.filter((item)=> item.user_id === props.userData.id).map((singleItem) => {
+                        return (
+                    <div className="d-flex align-items-center mb-5" key={singleItem.id}>
                       <div className="flex-shrink-0">
                         <MDBCardImage
-                          src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/E-commerce/Products/13.webp"
+                          src={singleItem.image}
                           fluid
                           style={{ width: "150px" }}
                           alt="Generic placeholder image"
@@ -44,30 +102,34 @@ const CartCheckout = () => {
                           <MDBIcon fas icon="times" />
                         </a>
                         <MDBTypography tag="h5" className="text-primary">
-                          Samsung Galaxy M11 64GB
-                        </MDBTypography>
-                        <MDBTypography tag="h6" style={{ color: "#9e9e9e" }}>
-                          Color: white
+                          {singleItem.brand} {singleItem.name}
                         </MDBTypography>
 
                         <div className="d-flex align-items-center">
-                          <p className="fw-bold mb-0 me-5 pe-3">799$</p>
+                          <p className="fw-bold mb-0 me-5 pe-3">${singleItem.price}</p>
 
                           <div className="def-number-input number-input safari_only">
-                            <button className="minus"></button>
+                            <button className="minus" onClick={minusQuantity}></button>
                             <input
                               className="quantity fw-bold text-black"
-                              min={0}
-                              defaultValue={1}
+                              min={1}
+                              max={20}
+                              value={cartQuantity}
                               type="number"
+                              onChange={quantityAddOrMinus}
                             />
-                            <button className="plus"></button>
+                            <button className="plus" onClick={addQuantity}></button>
                           </div>
+
+                          <p className="fw-bold mb-0 me-5 pe-3"> = ${(singleItem.price * cartQuantity).toFixed(2)}</p>
                         </div>
                       </div>
                     </div>
-
-                    <div className="d-flex align-items-center mb-5">
+                        ) 
+                    }) : <h1> No Items in your cart. </h1>
+                }
+                    
+                    {/* <div className="d-flex align-items-center mb-5">
                       <div className="flex-shrink-0">
                         <MDBCardImage
                           src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/E-commerce/Products/6.webp"
@@ -141,7 +203,7 @@ const CartCheckout = () => {
                           </div>
                         </div>
                       </div>
-                    </div>
+                    </div> */}
 
                     <hr
                       className="mb-4"
@@ -152,10 +214,10 @@ const CartCheckout = () => {
                       }}
                     />
 
-                    <div className="d-flex justify-content-between px-x">
-                      <p className="fw-bold">Discount:</p>
+                    {/* <div className="d-flex justify-content-between px-x">
+                      <p className="fw-bold">Sub:</p>
                       <p className="fw-bold">95$</p>
-                    </div>
+                    </div> */}
                     <div
                       className="d-flex justify-content-between p-2 mb-2"
                       style={{ backgroundColor: "#e1f5fe" }}
@@ -171,14 +233,14 @@ const CartCheckout = () => {
                   <MDBCol lg="5" className="px-5 py-4">
                     <MDBTypography
                       tag="h3"
-                      className="mb-5 pt-2 text-center fw-bold text-uppercase"
+                      className="mb-3 pt-2 text-center fw-bold text-uppercase"
                     >
                       Payment
                     </MDBTypography>
 
-                    <form className="mb-5">
+                    <form className="mb-3">
                       <MDBInput
-                        className="mb-5"
+                        className="mb-3"
                         label="Card number"
                         type="text"
                         size="lg"
@@ -186,7 +248,7 @@ const CartCheckout = () => {
                       />
 
                       <MDBInput
-                        className="mb-5"
+                        className="mb-3"
                         label="Name on card"
                         type="text"
                         size="lg"
@@ -194,9 +256,9 @@ const CartCheckout = () => {
                       />
 
                       <MDBRow>
-                        <MDBCol md="6" className="mb-5">
+                        <MDBCol md="6" className="mb-3">
                           <MDBInput
-                            className="mb-4"
+                            className="mb-3"
                             label="Expiration"
                             type="text"
                             size="lg"
@@ -206,11 +268,11 @@ const CartCheckout = () => {
                             placeholder="MM/YYYY"
                           />
                         </MDBCol>
-                        <MDBCol md="6" className="mb-5">
+                        <MDBCol md="6" className="mb-3">
                           <MDBInput
-                            className="mb-4"
-                            label="Cvv"
-                            type="text"
+                            className="mb-3"
+                            label="CVV"
+                            type="password"
                             size="lg"
                             minLength="3"
                             maxLength="3"
@@ -220,12 +282,54 @@ const CartCheckout = () => {
                         </MDBCol>
                       </MDBRow>
 
-                      <p className="mb-5">
-                        Lorem ipsum dolor sit amet consectetur, adipisicing elit
-                        <a href="#!"> obcaecati sapiente</a>.
-                      </p>
+                    <MDBTypography
+                      tag="h3"
+                      className="mb-3 pt-2 text-center fw-bold text-uppercase"
+                    > Shipping 
+                    </MDBTypography>
 
-                      <MDBBtn block size="lg">
+                    <MDBInput
+                        className="mb-3"
+                        label="Street Address"
+                        type="text"
+                        size="lg"
+                        placeholder=""
+                        defaultValue=""
+                      />
+
+                      <MDBInput
+                        className="mb-3"
+                        label="Apt or Suite #"
+                        type="text"
+                        size="lg"
+                        placeholder=""
+                        defaultValue=""
+                      />
+
+                      <MDBRow>
+                        <MDBCol md="6" className="mb-3">
+                          <MDBInput
+                            className="mb-3"
+                            label="City"
+                            type="text"
+                            size="lg"
+                            defaultValue=""
+                            placeholder=""
+                          />
+                        </MDBCol>
+                        <MDBCol md="6" className="mb-3">
+                          <MDBInput
+                            className="mb-3"
+                            label="State"
+                            type="text"
+                            size="lg"
+                            placeholder=""
+                            defaultValue=""
+                          />
+                        </MDBCol>
+                      </MDBRow>
+
+                    <MDBBtn block size="lg" className="mb-5">
                         Buy now
                       </MDBBtn>
 
@@ -234,10 +338,10 @@ const CartCheckout = () => {
                         className="fw-bold mb-5"
                         style={{ position: "absolute", bottom: "0" }}
                       >
-                        <a href="#!">
+                        <Link to='/'>
                           <MDBIcon fas icon="angle-left me-2" />
                           Back to shopping
-                        </a>
+                        </Link>
                       </MDBTypography>
                     </form>
                   </MDBCol>

d306ba27001016c830b0688677fbea6d164aa0d7
Josue Carreon <ijosue.carreon@gmail.com>
2023-04-10 10:43:30 -0500

    added swiper to homepage.jsx

--- src/components/homepage.jsx
+++ src/components/homepage.jsx
@@ -1,12 +1,12 @@
-import React from "react";
-import {
-    MDBCarousel,
-    MDBCarouselItem
-} from "mdb-react-ui-kit";
+import React, { useEffect } from "react";
+import { Swiper, SwiperSlide, useSwiper } from 'swiper/react';
+// import Swiper and modules styles
+import 'swiper/css';
 
-import { useState, useEffect } from "react";
+// import { useState } from "react";
 
 const Homepage = (props) => {
+    const swiper = useSwiper();
     const { setIsLoggedIn, getProductData } = props;
 
     useEffect(() => {
@@ -20,53 +20,59 @@ const Homepage = (props) => {
         }
     }, [])
 
+    const handleNextSlide = (swiper) => {
+        swiper.slideNext();
+    };
+
     return (
         <section>
-            {/* <div className="homePageFlex">
+            <div className="homePageFlex">
                 <div className="hp-text-container">
                     <h1 className="hpt"> Welcome to the Gutterball Bowling Superstore! </h1>
                     <h3 className="hpt"> Please login or create an account above to place an order or feel free to browse our selection of products.</h3>
                     <h3 className="hpt"> Thank you for your business. </h3>
                 </div>
-            </div> */}
-            {/* <div> */}
-                <MDBCarousel showIndicators showControls fade className="homePageFlex">
-                    <MDBCarouselItem 
-                    // className="carousel-item active" id="carousel-item-1"
-                        className='w-30 d-block'
-                        itemId={1}
-                        src='/images/balls/Brunswick-HP-Defender.png' 
-                        style={{ width: "20%", height: "20%", textAlign: "center"}}
-                        alt=''
-                    >
-                        <h5>Original Price: $159</h5>
-                        <p>Sale Price: $24</p>
-                    </MDBCarouselItem>
-                    <MDBCarouselItem 
-                    // class="carousel-item" id="carousel-item-2"
-                        className='w-30 d-block'
-                        itemId={2}
-                        src='/images/balls/Brunswick-HP-Mindset.png' 
-                        style={{ width: "20%", height: '20%', textAlign: "center"}}
-                        alt=''
-                    >
-                        <h5>Original Price: $1,159</h5>
-                        <p>Sale Price: $69</p>
-                    </MDBCarouselItem>
-                    <MDBCarouselItem 
-                    // class="carousel-item" id="carousel-item-3"
-                        className='w-30 d-block'
-                        itemId={3}
-                        src='/images/balls/Brunswick-MP-Attitude.png' 
-                        style={{ width: "20%", height: "20%", textAlign: "center" }}
-                        alt=''
-                    >
-                        <h5>Original Price: $259</h5>
-                        <p>Sale Price: $12</p>
-                    </MDBCarouselItem>
-                </MDBCarousel>
-            {/* </div>            */}
+            </div>
+            <div className="button-container">
+                <button onClick={() => handleNextSlide(swiper)} >Slide to the next slide</button>
+            </div>
+            <Swiper
+                spaceBetween={50}
+                slidesPerView={1}
+                onSlideChange={() => console.log('slide change')}
+                onSwiper={(swiper) => console.log(swiper)}
+            >
+                <SwiperSlide>
+                    <img
+                        src="/images/balls/Brunswick-HP-Mindset.png"
+                        alt=""
+                        style={{ width: '20%' }}
+                    />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img
+                        src="/images/balls/Brunswick-MP-Attitude.png"
+                        alt=""
+                        style={{ width: '20%' }}
+                    />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img
+                        src="/images/balls/Brunswick-HP-Defender.png"
+                        alt=""
+                        style={{ width: '20%' }}
+                    />
+                </SwiperSlide>
+                <SwiperSlide>Slide 4</SwiperSlide>
+            </Swiper>
         </section>
     )
 }
 export default Homepage;
+
+// '/images/balls/Brunswick-HP-Mindset.png' 
+// '/images/balls/Brunswick-MP-Attitude.png' 
+// '/images/balls/Brunswick-HP-Defender.png' 
+
+// <h5>Original Price: $259</h5>
+// <p>Sale Price: $12</p>
